b4dc472edd51015bbe0bf5983b8e46076829cd15
Yumiko-lab <1032816215@qq.com>
2024-10-17 15:34:36 +0800

    moidfy Floder name

---
+++ Math/Probability and Mathematical Expectation/Luogu/P2719.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed << setprecision(4);
+
+    int n;
+    cin >> n;
+    
+
+    return 0;
+}
