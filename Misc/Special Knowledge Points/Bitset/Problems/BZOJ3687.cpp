55c9bb09a5ef7cbf44d9eb16178c3b2183ce289d
Yumiko-lab <1032816215@qq.com>
2024-10-19 12:24:37 +0800

    add

---
+++ Misc/Special Knowledge Points/Bitset/Problems/BZOJ3687.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    constexpr int M = 200010;
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    vector<int> f(M);
+    f[0] = 1;
+    for (auto x : a) {
+        for (int j = M - 1; j >= x; j--) {
+            f[j] ^= f[j - x];
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < M; i++) {
+        if (f[i]) {
+            ans ^= i;
+        }
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
---
+++ Misc/Special Knowledge Points/Bitset/Problems/LG_P10480.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <bitset>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> adj(n);
+    vector<int> din(n);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--, v--;
+        adj[u].emplace_back(v);
+        din[v]++;
+    }
+
+    vector<int> seq;
+    auto topo = [&]() {
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (din[i] == 0) {
+                q.emplace(i);
+            }
+        }
+        while (!q.empty()) {
+            auto u = q.front();
+            q.pop();
+            seq.emplace_back(u);
+            for (auto v : adj[u]) {
+                if (--din[v] == 0) {
+                    q.emplace(v);
+                }
+            }
+        }
+    };
+
+    topo();
+
+    bitset<30010> f[n]{};
+
+    for (int i = seq.size() - 1; i >= 0; i--) {
+        int u = seq[i];
+        f[u][u] = 1;
+        for (auto v : adj[u]) {
+            f[u] |= f[v];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << f[i].count() << '\n';
+    }
+
+    return 0;
+}
