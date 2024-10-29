51785d31a94d536f2f2e0ff77b7e3975faf4b8db
Yumiko-lab <1032816215@qq.com>
2024-10-29 19:31:50 +0800

    modify LG P1962

--- Topics/Math/Matrix Multiplication/Problems/LG_P1962.cpp
+++ Topics/Math/Matrix Multiplication/Problems/LG_P1962.cpp
@@ -1,43 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+using i64 = long long;
 
 constexpr int N = 2, MOD = 1'000'000'007;
 
-int n;
-int res[N] = {1, 1}; // {f[n], f[n + 1]}
+i64 n;
+int res[N][N] = {1, 1}; // {f[n], f[n + 1]}
 int A[N][N] = {
     {0, 1},
     {1, 1}
 };
 
-void mul(int c[][N], int a[][N], int b[][N]) {
+void mul(int c[][N], int a[][N], int b[][N]) { // c = a * b
     static int temp[N][N] {};
     memset(temp, 0, sizeof temp);
-    
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             for (int k = 0; k < N; k++) {
-                temp[i][j] = (temp[i][j] + 1LL * a[i][k] + b[k][j] % MOD) % MOD;
+                temp[i][j] = (temp[i][j] + 1LL * a[i][k] * b[k][j] % MOD) % MOD;
             }
         }
     }
     memcpy(c, temp, sizeof temp);
 }
 
-void mul(int c[N], int a[N], int b[][N]) {
-    static int temp[N] {};
-    memset(temp, 0, sizeof temp);
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            temp[i] = (temp[i] + 1LL * a[j] * b[j][i] % MOD) % MOD;
-        }
-    }
-    memcpy(c, temp, sizeof temp);
-}
-
 void power() {
-    int k = n - 1;
+    i64 k = n - 1;
     while (k) {
         if (k & 1) {
             mul(res, res, A);
@@ -53,7 +42,9 @@ int main() {
 
     cin >> n;
 
-    cout << res[0] << '\n';
+    power();
+
+    cout << res[0][0] << '\n';
 
     return 0;
 }
